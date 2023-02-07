b8afd5db0da1a959f3865f435f71d7ed25f9640a
Ethan Pierce <ethan.pierce@colorado.edu>
2023-02-07 12:40:59 -0700

    Add test data, config routines

--- src/Ku.py
+++ src/Ku.py
@@ -1,23 +1,64 @@
 import numpy as np
 import xarray as xr
+import tomli
 
 class Ku_model:
 
     def __init__(self):
-        pass
+        self.experiment = ""
+        self.inputs_dir = ""
+        self.outputs_dir = ""
+        self.number_of_years = 0
+        self.grid_shape = [0, 0]
+        self.input_files = {}
+        self.constants = {}
     
 ##############
 # Initialize #
 ##############
 
-    def read_inputs(self):
-        pass
+    def read_config(self, config_file: str):
 
-    def setup_soil_textures(self):
-        pass
+        with open(config_file, "rb") as file:
+            config = tomli.load(file)
 
-    def broadcast_scalars(self):
-        pass 
+        self.experiment = config['experiment']
+        self.inputs_dir = config['directories']['inputs_dir']
+        self.outputs_dir = config['directories']['outputs_dir']
+
+        self.number_of_years = config['domain']['number_of_years']
+        self.grid_shape = config['domain']['grid_shape']
+
+        self.input_files = {var: ncfile for var, ncfile in config['files'].items()}
+        self.constants = {var: val for var, val in config['constants'].items()}
+
+    def read_input_files(self):
+        if len(self.input_files) == 0:
+            raise ValueError("No input files to read: did you call read_config() first?")
+
+        for var, file in self.input_files.items():
+            data = xr.open_dataarray(file)
+
+            if data.shape == (self.number_of_years, self.grid_shape[0], self.grid_shape[1]):
+                pass
+
+            elif data.shape == (self.grid_shape[0], self.grid_shape[1]):
+                data = data.expand_dims({"time": self.number_of_years}, axis = 0)
+
+            elif data.shape == (self.number_of_years,):
+                data = data.expand_dims({"x": self.grid_shape[1], "y": self.grid_shape[0]}, axis = [1, 2])
+
+            elif data.shape == (1,):
+                dim_name = str(data.dims[0])
+                data = data.expand_dims({"time": self.number_of_years, 
+                                         "x": self.grid_shape[1], 
+                                         "y": self.grid_shape[0]},
+                                         axis = [0, 1, 2])
+                data = data.squeeze(dim_name)
+
+            else:
+                raise ValueError(var + " data cannot be broadcast to shape " + 
+                                 str((self.number_of_years, self.grid_shape[0], self.grid_shape[1])))
 
 ##########
 # Update #
--- test/test_Ku.py
+++ test/test_Ku.py
@@ -8,5 +8,23 @@ def test_always_passes():
 def Ku():
     return Ku_model()
 
-def test_read_inputs(Ku):
-    Ku.read_inputs()
+def test_read_config(Ku):
+    Ku.read_config("./test/config.toml")
+    
+    assert Ku.experiment == 'test'
+    assert Ku.inputs_dir == './test/data/inputs'
+    assert Ku.outputs_dir == './test/data/outputs'
+    assert Ku.number_of_years == 100
+    assert Ku.grid_shape == [100, 100]
+    assert len(Ku.input_files) == 10
+    assert len(Ku.constants) == 4
+
+class TestReadInputs:
+
+    def test_throw_error_if_no_files_list(self, Ku):
+        with pytest.raises(ValueError):
+            Ku.read_input_files()
+    
+    def test_read_inputs(self, Ku):
+        Ku.read_config("./test/config.toml")
+        Ku.read_input_files()
