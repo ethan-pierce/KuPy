38a295e452b4d04f9f5b1fc3fa418f9464a34dc1
Ethan Pierce <ethan.pierce@colorado.edu>
2023-02-22 13:27:26 -0700

    Add BMI functions

--- src/bmi_Ku.py
+++ src/bmi_Ku.py
@@ -24,14 +24,15 @@ SOFTWARE.
 *
 """
 
-from Ku import Ku_model
+import numpy as np
+from src.Ku import Ku_model
 
 class BmiKuModel:
     """Basic model interface for the Kudryavstev permafrost model."""
 
     _name = 'Kudryavtsev Permafrost Model'
 
-    _input_var_names = {
+    _input_var_names = [
         'air_temperature',
         'temperature_amplitude',
         'snow_thickness',
@@ -41,14 +42,14 @@ class BmiKuModel:
         'thawed_vegetation_height',
         'frozen_vegetation_diffusivity',
         'thawed_vegetation_diffusivity'
-    }
+    ]
 
-    _output_var_names = {
+    _output_var_names = [
         'permafrost_temperature',
         'active_layer_thickness'
-    }
+    ]
 
-    _var_units = {
+    _var_units_map = {
         'air_temperature': 'degrees C',
         'temperature_amplitude': 'degrees C',
         'snow_thickness': 'meters',
@@ -73,6 +74,10 @@ class BmiKuModel:
         
         self._start_time = 0
         self._end_time = None
+        self._current_time = 0
+
+        # Using 'years' as a time unit is generally not preferred
+        # However, the Ku model does not support ANY time step other than 1 year
         self._time_units = 'years'
 
     def initialize(self, config_file: str):
@@ -85,8 +90,124 @@ class BmiKuModel:
         self._model = Ku_model()
 
         # Initialization routines
-        self._model.initialize(config_file = config_file)
+        self._model.read_config(config_file = config_file)
         self._model.read_input_files()
 
+        self._values = {
+            var: getattr(self._model, var).values for var in self._input_var_names
+        }
+        for var in self._output_var_names:
+            self._values[var] = np.empty((
+                self._model.number_of_years, 
+                self._model.grid_shape[0],
+                self._model.grid_shape[1]
+            ))
 
+        self._var_units = self._var_units_map.copy()
+
+        self._var_loc = {
+            var: 'node' for var in self._input_var_names + self._output_var_names
+        }
+
+        self._grids = {
+            i: list(self._var_units_map.keys())[i] for i in range(len(self._var_units_map.keys())) 
+        }
+
+        self._grid_type = {
+            i: 'uniform_rectilinear' for i in range(len(self._var_units_map.keys())) 
+        }
+
+        self._start_time = 0
+        self._end_time = self._model.number_of_years
+
+    def update(self):
+        """Run the model for the current time step."""
+        self._model.run_one_step(self._current_time)
+        self._current_time += 1
+
+    def update_until(self, end_time: int):
+        """Update the model until a certain year (inclusive)."""
+        years = self._current_time + end_time
+
+        for t in np.arange(self._current_time, end_time + 1, 1):
+            self._model.run_one_step(t)
+
+        self._current_time = end_time
+
+    def finalize(self, path_to_output = None):
+        """If specified, write output to a netcdf file."""
+
+        if path_to_output is not None:
+            self._model.write_output(path_to_output, vars_to_write = self._output_var_names)
+
+    def get_component_name(self) -> str:
+        """Return the name of the component."""
+        return self._name
+
+    def get_input_item_count(self) -> int:
+        """Return the number of input variables."""
+        return len(self._input_var_names)
+
+    def get_output_item_count(self) -> int:
+        """Return the number of output variables."""
+        return len(self._output_var_names)
+
+    def get_input_var_names(self) -> list:
+        """Return a list of input variables."""
+        return self._input_var_names
+    
+    def get_output_var_names(self) -> list:
+        """Return a list of output variables."""
+        return self._output_var_names
+
+    def get_var_grid(self, var: str) -> int:
+        """Return the grid ID for a variable."""
+        for grid, variables in self._grids.items():
+            if var in variables:
+                return grid 
+
+    def get_var_type(self, var: str) -> str:
+        """Return the data type of a variable."""
+        return str(self.get_value(var).dtype)
+
+    def get_var_units(self, var: str) -> str:
+        """Return the units of a variable."""
+        return self._var_units[var]
+
+    def get_var_nbytes(self, var: str) -> int:
+        """Return the size of a variable in bytes."""
+        return self.get_value(var).nbytes
+
+    def get_var_itemsize(self, var: str) -> int:
+        """Return the size of one element of a variable in bytes."""
+        return np.dtype(self.get_var_type(var)).itemsize
+
+    def get_var_location(self, var: str) -> str:
+        """Returns the location of a variable on the grid: either 'node', 'edge', or 'face'."""
+        return 'node'
+
+    def get_current_time(self) -> int:
+        """Return the current time."""
+        return self._current_time
+
+    def get_start_time(self) -> int:
+        """Return the start time."""
+        return self._start_time
+
+    def get_end_time(self) -> int:
+        """Return the end time."""
+        return self._end_time
+
+    def get_time_units(self) -> str:
+        """Return the time units of the model."""
+        return self._time_units
+
+    def get_time_step(self) -> str:
+        """Return the model's time step."""
+        return 1
+
+    
+
+    
+        
 
---
+++ test/test_bmi_Ku.py
@@ -0,0 +1,48 @@
+import pytest
+from src.bmi_Ku import BmiKuModel
+from numpy.testing import assert_approx_equal
+
+def test_always_passes():
+    assert True
+
+@pytest.fixture
+def Init():
+    return BmiKuModel()
+
+def test_bmi_fixture_init(Init):
+    assert Init._model == None
+
+def test_bmi_fixture_initialize(Init):
+    test_config = './test/config.toml'
+    Init.initialize(test_config)
+
+    assert len(Init._values.keys()) == 11
+
+    for var in Init._input_var_names:
+        assert Init._values[var].shape == (100, 100, 100)
+    for var in Init._output_var_names:
+        assert Init._values[var].shape == (100, 100, 100)
+
+    for var in Init._input_var_names:
+        assert var in Init._grids.values()
+    for var in Init._output_var_names:
+        assert var in Init._grids.values()
+        
+@pytest.fixture
+def Bmi():
+    test_config = './test/config.toml'
+    fixture = BmiKuModel()
+    fixture.initialize(test_config)
+    return fixture
+
+def test_bmi_update(Bmi):
+    Bmi.update()
+
+    assert_approx_equal(Bmi._model.permafrost_temperature[0, 0], -3.006, significant=4)
+    assert_approx_equal(Bmi._model.active_layer_thickness[0, 0], 1.226, significant=4)
+
+def test_bmi_update_until(Bmi):
+    Bmi.update_until(10)
+
+    assert_approx_equal(Bmi._model.permafrost_temperature[0, 0], -1.7699, significant=4)
+    assert_approx_equal(Bmi._model.active_layer_thickness[0, 0], 1.318, significant=4)
